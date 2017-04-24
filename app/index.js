c35795b914ca269a9ca83b570b7414e4d7d93545
BadWaka <1456683844@qq.com>
2017-04-24 22:45:56 +0800

    initial

---
+++ app/index.js
@@ -0,0 +1,13 @@
+function component() {
+    var element = document.createElement('div');
+
+    element.innerHTML = _.join([
+        'Hello', 'webpack'
+    ], '');
+
+    return element;
+}
+
+document
+    .body
+    .appendChild(component());
